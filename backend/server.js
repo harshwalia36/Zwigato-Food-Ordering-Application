59e46e554331e711a2e993eb8efecbd4afdbd582
harshwalia36 <harsh.p.walia@gmail.com>
2024-12-21 22:27:50 +0530

    Add onboarding restaurants dummy route and connect with mongodb database.

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,29 @@
 require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
+const onboardingRestaurantsRoutes = require('./routes/onboarding_restaurants');
 
 const app = express();
 
-
-app.get('/', (req, res) => {
-    res.send('Hello, world!');
+//middleware
+app.use(express.json());
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
 });
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-});
+app.use('/partner-with-us/new', onboardingRestaurantsRoutes);
+
+console.log(process.env.MONGO_URI);
+//connect to mongodb
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        //listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log(`Connected to MongoDb and Server is running on port ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log('Error connecting to MongoDB', err);
+    });
+
